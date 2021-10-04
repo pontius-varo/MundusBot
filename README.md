2039b6263de7bdcf8713dd7228422268c085434a
Antonio Ponce <zponce@posteo.net>
2021-10-04 18:30:47 -0400

    Overhaul MELTY

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # MundusBot
 
+Version: 1.0 "Melty"
+
 A telegram bot written in Python 3.9 for a private server.
 
 Uses the following libraries: sqlite3, python-telegram-bot
-
-
-
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 # MundusBot script #
 
 # Imports
-import utilities, logging
+import utilities, logging, templates
 from telegram.ext import CommandHandler, Updater
 
+from random import randint
+
 # Establish connection to db
-myconnection = utilities.create_connection("db/database.db")
+myconnection = utilities.create_connection("db/neodatabase")
 
 # Updater
 updater = Updater(token='1932470254:AAF2Xtxpgu0k_NkIAcIOkuVVsTT3i5Nb744', use_context=True)
@@ -16,67 +18,117 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # Start function that also processes updates
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Et tu, Josh?")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I am online!")
 
 def getuser(update, context):
     username = context.args
     try:
-        usrinfo = utilities.cleanstring(str(utilities.execute_read_query(myconnection, ("select url1 from main where user=\'%s\'" % username[0]))))
-        context.bot.send_message(chat_id=update.effective_chat.id, text=('%s\n%s' % (username[0], usrinfo)))
+        tempdata = utilities.cleanstring(str(utilities.execute_read_query(myconnection, ("select Url1, Url2, Url3 from Userlib where Username = \'%s\';" % username[0]))))
+        # This should return a clean array!
+        userdata = tempdata.split(" ")
+
+        count = 0
+
+        # Check the amount of Nones!
+        for x in userdata:
+
+            if(x == "None"):
+                count += 1
+            else:
+                pass
+
+        if(username[0] == "@eatliftprogram"):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(templates.responses['SPECIAL'] % (username[0], userdata[0], userdata[1], userdata[2])))
+        elif(username[0] == "@pavlogetsit"):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(templates.responses['SPECIAL2'] % (username[0], userdata[0], userdata[1], userdata[2])))
+
+        elif(username[0] == "@aurelianus_varo"):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(templates.responses['SPECIAL3'] % (username[0], userdata[0], userdata[1], userdata[2])))
+
+        elif(count > 2):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(templates.responses['FORIEGNER'] % (username[0])))
+
+        elif(count == 2):
+            msg = (templates.responses['PLEB'] % (username[0], userdata[0]))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(msg))
+
+        elif(count == 1):
+            msg = (templates.reponses['EQUESTRIAN'] % (username[0], userdata[0], userdata[1]))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(msg))
+
+        else:
+            msg = (templates.responses['PATRICIAN'] % (username[0], userdata[0], userdata[1], userdata[2]))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=(msg))
 
     except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Not found')
-        print("User not found, are you sure it exists?")
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Not Found. That user doesn\'t exist m8.')
 
 # Add user to db and returns whether or not it was successful
 def adduser(update, context):
-    username = context.args
-    usr = username[0]
-    #if(execute_read_query(myconnection, ("select user from main where user=%s"% username[0]))):
-    #       context.bot.send_message(chat_id=update.effective_chat.id, text=('Duplicate username detected!'))
-    #else:
-    try:
-        utilities.execute_query(myconnection, ("insert into main(user) values (\'%s\');" % usr))
-    except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Error:{e}')
+    #username = context.args
+    #usr = username[0]
+    usr = ('@%s' % (update.message.from_user.username))
+    if(str(utilities.execute_read_query(myconnection, (templates.queries['DOESUSEREXIST'] % (usr)))) == "[]"):
+        # Hardcoded I know, but it works
+        userint = randint(1, 10000)
+        try:
+             utilities.execute_query(myconnection, (templates.queries['NEWUSER'] % (str(userint), usr)))
+             context.bot.send_message(chat_id=update.effective_chat.id, text=("%s added successfully to the database." % usr))
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Error')
+    else:
+       context.bot.send_message(chat_id=update.effective_chat.id, text=('That user already exists m8.'))
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=("%s added successfully to the database. Boom baby!" % username[0]))
-
-# Add URL/information to db
 def addinfo(update, context):
-    username = context.args[0]
+    username = ('@%s' % (update.message.from_user.username))
+    entry_type = context.args[0]
     url = context.args[1]
-    # Put code to check for dupes here!
-    try:
-        utilities.execute_query(myconnection, ("update main set url1=(\'%s\') where user=\'%s\'" % (url, username)))
-        context.bot.send_message(chat_id=update.effective_chat.id, text=('%s\'s url was successfully added to the database.' % username))
-    except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Error: {e}')
-        print('You might want to fix this!')
+
+    if entry_type not in templates.catagories:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=('Invalid catagory m8.'))
+        return
+
+    if(utilities.cleanstring(str((utilities.execute_read_query(myconnection, (templates.queries['DOESUSEREXIST'] % (username)))))) == username):
+        try:
+            utilities.execute_query(myconnection, (templates.queries['ADDURL'] % (templates.catagory_pairs[entry_type], url, username)))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=('Your %s url has been updated, %s' % (entry_type, username)))
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Error')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=('You\'re not in my datalibrary, GTFO.'))
 
 # Update Status [Adds a string to the 'status' column in the main table]
 def updatestatus(update, context):
-    username = context.args[0]
-    status_str = utilities.tostring(utilities.newarray(context.args))
-    try:
-        utilities.execute_query(myconnection, ("update main set status=(\'%s\') where user=\'%s\'" %(status_str, username)))
-    except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='An error occured')
-        print("An error occured")
+    username = ('@%s' % (update.message.from_user.username))
+    content = context.args[0]
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Your status has been set.')
+    if(utilities.cleanstring(str((utilities.execute_read_query(myconnection, (templates.queries['DOESUSEREXIST'] % (username)))))) == username):
+        try:
+            utilities.execute_query(myconnection, (templates.queries['ADDSTATUS'] % (content, username)))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=('Your status has been set, %s' % (username)))
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Error')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=('You\'re not in my datalibrary, GTFO.'))
 
+# Get User Status from Database
 def getstatus(update, context):
-    username = context.args
+    username = context.args[0]
+    if(utilities.cleanstring(str((utilities.execute_read_query(myconnection, (templates.queries['DOESUSEREXIST'] % (username)))))) == username):
+        try:
+            status = utilities.cleanstring(str(utilities.execute_read_query(myconnection, (templates.queries['GETSTATUS'] % (username)))))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=('%s\'s current status: %s' % (username, status)))
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Error')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=('That user doesn\'t exist m8'))
+
+# Help
+def gethelp(update, context):
     try:
-        mystatus = utilities.cleanstring(str(utilities.execute_read_query(myconnection, ("select status from main where user=\'%s\'" % username[0]))))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=(templates.help_txt[0]))
     except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='An error occured')
-        print("An error occured")
-
-    context.bot.send_message(chat_id=update.effective_chat.id, text=('%s\'s status:\n%s' % (username[0], mystatus)))
-
-# Handlers #
+        print('Error')
 
 # Start Handler
 start_handler = CommandHandler('start', start)
@@ -88,7 +140,7 @@ dispatcher.add_handler(get_userhandler)
 adduser_handler = CommandHandler('adduser', adduser, pass_args=True)
 dispatcher.add_handler(adduser_handler)
 # Add Info Handler
-addinfo_handler = CommandHandler('addinfo', addinfo, pass_args=True)
+addinfo_handler = CommandHandler('updateurl', addinfo, pass_args=True)
 dispatcher.add_handler(addinfo_handler)
 
 #  Update Status Handler
@@ -98,6 +150,10 @@ dispatcher.add_handler(update_handler)
 # Get Status Handler
 get_handler = CommandHandler('getstatus', getstatus)
 dispatcher.add_handler(get_handler)
+
+# Help Handler
+help_handler = CommandHandler('help', gethelp)
+dispatcher.add_handler(help_handler)
 
 # End of Line
 updater.start_polling()
